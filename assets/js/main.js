d67c5e37940452eebb1f955784a53b931f419570
SATYAM TIWARI <itssatyam.345@gmail.com>
2023-05-17 17:39:29 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,6 +31,20 @@ function linkAction()
 
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
+function submitForm() {
+    // Get form values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+  
+    // Perform form validation here (e.g., check if required fields are filled)
+  
+    // Process the form data (e.g., send it to a server using AJAX)
+  
+    // Display a success message or perform any desired action
+    alert("Form submitted successfully!");
+  }
+
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal(
@@ -64,3 +78,23 @@ sr.reveal('.skills__img',{delay: 600});
 
 /*SCROLL WORK*/
 sr.reveal('.work__img',{interval: 200}); 
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
